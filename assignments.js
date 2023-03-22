bb9340de7e57bc860bd586fbe6e3c1544d86cba6
Jdizzydizzle <127226946+Jdizzydizzle@users.noreply.github.com>
2023-03-22 17:10:40 +0000

    init

---
+++ assignments.js
@@ -0,0 +1,19 @@
+
+let country = "Ireland";
+//let continent = "Europe";
+let population = 4000000;
+let isIsland = true;
+let language;
+
+console.log(language);
+console.log(typeof country);
+//console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,37 @@
+/* let js = 'amazing';
+ console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+
+// variable name conventions
+let firstname = "Matilda";
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+ let myfirstjob = "coder";
+console.log(myfirstjob);
+*/
+
+/*
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+
+javaScriptIsFun = 'Yes!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); 
+*/
