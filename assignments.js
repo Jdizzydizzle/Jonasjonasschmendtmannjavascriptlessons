7378a92754359fc773ecf4b34fe4dbc58a0f0eaf
John Walshe <127226946+Jdizzydizzle@users.noreply.github.com>
2023-03-23 19:35:57 +0000

    Add files via upload

--- assignments.js
+++ assignments.js
@@ -1,19 +1,24 @@
 
 const country = "Ireland";
-//let continent = "Europe";
+let continent = "Europe";
 let population = 4000000;
 const isIsland = true;
 const language = "English";
-language = "Irish";
+// population++;
+const FinlanPopulation = 6000000;
+const AvPopulation = 33000000;
+const description = 'Portugal is in Europe, and its 11 million people speak Portugese';
 
-console.log(language);
+
+/* console.log(language);
 console.log(typeof country);
-//console.log(typeof continent);
+console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof isIsland);
-console.log(typeof language);
+console.log(typeof language); */
 
-
+console.log(population);
+console.log(population < AvPopulation);
 
 
 
