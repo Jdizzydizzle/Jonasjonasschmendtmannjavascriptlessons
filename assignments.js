d83623cfcac768475f6d749883a9ac8cbc6c7086
John Walshe <127226946+Jdizzydizzle@users.noreply.github.com>
2023-03-25 17:40:32 +0000

    Add files via upload

--- assignments.js
+++ assignments.js
@@ -1,7 +1,7 @@
 
-const country = "Ireland";
+/*const country = "Ireland";
 let continent = "Europe";
-let population = 4000000;
+let population = 130000000;
 const isIsland = true;
 const language = "English";
 // population++;
@@ -9,16 +9,63 @@ const FinlanPopulation = 6000000;
 const AvPopulation = 33000000;
 const description = 'Portugal is in Europe, and its 11 million people speak Portugese';
 
+if (population > 33000000) {
+    console.log('Irelands population is above average');
+} else {
+    console.log(`Irelands population is ${33000000 - population} below average`);
+}
 
-/* console.log(language);
+console.log(`${country} is in ${continent}, and its ${population} people speak ${language} `);
+console.log(language);
 console.log(typeof country);
 console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof isIsland);
-console.log(typeof language); */
+console.log(typeof language);
 
 console.log(population);
-console.log(population < AvPopulation);
+console.log(population < AvPopulation); */
+
+
+/*const marksMass = 78;
+const marksHeight = 1.69;
+const johnsMass = 92;
+const johnsheight = 1.95; */
+
+const marksMass = 95;
+const marksHeight = 1.88;
+const johnsMass = 85;
+const johnsheight = 1.76;
+
+const MarksBMI = marksMass / (marksHeight * marksHeight);
+console.log(MarksBMI);
+
+const JohnsBMI = johnsMass / (johnsheight * johnsheight);
+console.log(JohnsBMI);
+
+let markHigherBMI = MarksBMI > JohnsBMI;
+console.log(MarksBMI, JohnsBMI, markHigherBMI);
+
+if (MarksBMI > JohnsBMI) {
+    console.log(`Mark's BMI ${MarksBMI} is higher than Johns BMI ${JohnsBMI}!`);
+}
+else {
+    console.log(`John's BMI ${JohnsBMI} is higher than Marks BMI ${JohnsBMI}!`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- script.js
+++ script.js
@@ -33,8 +33,8 @@ console.log(typeof year);
 year = 1991;
 console.log(typeof year);
 
-console.log(typeof null); 
-*/
+console.log(typeof null);
+
 
 let age = 30;
 age = 31;
@@ -46,3 +46,208 @@ const birthyear = 1991;
 
 var job = 'programmer';
 job = 'teacher';
+*/
+
+// math operators
+
+/* console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3); */
+// means 2 to the power of 3 which is equals to 2*2*2
+
+// assignment operators
+//const firstname = 'jonas';
+//const lastname = 'Schmedtmann';
+// console.log(firstname + ' ' + lastname);
+
+/* let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x*4 = 100
+x++; // x + 1
+x--;
+// x--;
+console.log(x);
+
+// comparison operators
+
+console.log(ageJonas > ageSarah); // >, <, <=, >=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18; 
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x=y=20 //x=10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge); */
+/*
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthyear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthyear) + ' year old '
+    + job + '!';
+// console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthyear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string....`);
+
+// console.log('String with \n\
+// multiple \n\
+// lines');
+
+console.log(`String with
+multiple
+lines`);   */
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving licence🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years:)`);
+}
+
+const birthyear = 2012;
+
+let century;
+if (birthyear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
